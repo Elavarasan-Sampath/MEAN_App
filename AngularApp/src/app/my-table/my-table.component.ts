8da226a5380fd24e2bf7421b4a1aa577732ccc39
Elavarasan-Sampath <elavarasan.sampath@gmail.com>
2019-04-19 20:06:09 +0530

    proxy

--- AngularApp/src/app/my-table/my-table.component.ts
+++ AngularApp/src/app/my-table/my-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatSort } from '@angular/material';
 import { MyTableDataSource } from './my-table-datasource';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'my-table',
@@ -14,6 +15,14 @@ export class MyTableComponent implements OnInit {
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['id', 'name','status'];
+  url: 'http://localhost:3000/api/contacts';
+  data:any;
+  constructor(private http: HttpClient) {
+    
+    this.http.get(this.url).subscribe((res : any) => {
+      this.data = res;
+    });
+  }
 
   ngOnInit() {
     this.dataSource = new MyTableDataSource(this.paginator, this.sort);
